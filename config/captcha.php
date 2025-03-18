0528854dfb1273809c0ca27264e27bfcf9c9e75d
namehuzaifa <nnaptech2@gmail.com>
2025-03-18 14:29:02 +0500

    captcha key change

--- config/captcha.php
+++ config/captcha.php
@@ -3,8 +3,10 @@
 return [
     // 'secret' => '6Le4XewqAAAAAMc088x7pngEBdcrhH3yEtj_fq6A',
     // 'sitekey' => '6Le4XewqAAAAAGgFtVFTjZzHnt9fQDGFH6_t1BKw',
-    'secret' => '6Le6lPUqAAAAAJQqv2gRL45tEoDSnC9VaqE3SN2E',
-    'sitekey' => '6Le6lPUqAAAAAP-YSbGwhf6dXml2Gshp6Zln0pQ8',
+    // 'secret' => '6Le6lPUqAAAAAJQqv2gRL45tEoDSnC9VaqE3SN2E',
+    // 'sitekey' => '6Le6lPUqAAAAAP-YSbGwhf6dXml2Gshp6Zln0pQ8',
+    'secret' => '6LeRhvcqAAAAAHtUXxAQWD6BtghASRpiNoDKlKJS',
+    'sitekey' => '6LeRhvcqAAAAAF_GhOaiYmowusiNAC9hdvpRStKz',
     'active' => true,
     'options' => [
         'timeout' => 30,
