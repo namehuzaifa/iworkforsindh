5daacec8c241facd07ca18f25892375d5b85ede4
namehuzaifa <nnaptech2@gmail.com>
2025-03-25 13:11:07 +0500

    changes

--- app/Mail/ApplyJobByEmail.php
+++ app/Mail/ApplyJobByEmail.php
@@ -42,12 +42,12 @@ class ApplyJobByEmail extends Mailable
     /**
      * Get the message content definition.
      */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
+    // public function content(): Content
+    // {
+    //     return new Content(
+    //         view: 'view.name',
+    //     );
+    // }
 
      /**
      * Build the message.
