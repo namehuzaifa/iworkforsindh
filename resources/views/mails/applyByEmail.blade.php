1695c3f5c5e175b75c69aeefbe5c71e8ba19ded0
namehuzaifa <nnaptech2@gmail.com>
2025-03-25 15:31:54 +0500

    changes

--- resources/views/mails/applyByEmail.blade.php
+++ resources/views/mails/applyByEmail.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
-Dear {{ $userName }}
-Thank you for applying for the position of {{ $jobTitle }} at {{ $companyName }}. We have received your application and it is currently under review.
-If your qualifications align with our requirements, we will be in touch regarding the next steps. Thank you for considering {{ $companyName }} as your potential employer.
-Thank you for choosing {{ config('app.name') }} 
+<h4>Dear {{ $userName }}</h4>
+<p>Thank you for applying for the position of {{ $jobTitle }} at {{ $companyName }}. We have received your application and it is currently under review.</p>
+<p>If your qualifications align with our requirements, we will be in touch regarding the next steps. Thank you for considering {{ $companyName }} as your potential employer.</p>
+<p>Thank you for choosing {{ config('app.name') }} </p>
 
 @endcomponent
