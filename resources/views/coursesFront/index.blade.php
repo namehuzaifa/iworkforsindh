1e4394d7cd25004c8ddc8f75fc69639bdcb88663
namehuzaifa <nnaptech2@gmail.com>
2026-01-26 16:39:06 +0500

    css changing  courses module

--- resources/views/coursesFront/index.blade.php
+++ resources/views/coursesFront/index.blade.php
@@ -52,6 +52,12 @@
         position: relative;
     }
 
+
+    .card-img-wrapper img {
+        width: 100%;
+        height: 100%;
+        object-fit: fill;
+    }
     .main-heading{
         font-family: 'Poppins', sans-serif !important;
     }
